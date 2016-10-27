7f6f63ed1b450ac78f70e08c91475f07243d6bc1
Kevin Guegan <k.guegan8@laposte.net>
2016-10-27 12:11:31 +0200

    Add somes pages quote creation/invoite list and co
    Creation somes pages
        - Quote Creation
        - Quote List
        - Invoite Creation
        - Invoite List

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -8,12 +8,52 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class AdminController extends Controller
 {
     /**
-     * Matches /blog exactly
+     * Administration Page
      *
      * @Route("/admin", name="admin")
      */
     public function indexAction()
     {
         return $this->render('@App/index.html.twig');
+    }
+
+    /**
+     * Create quote
+     *
+     * @Route("/admin/quote", name="quote")
+     */
+    public function createQuoteAction()
+    {
+        return $this->render('@App/createQuote.html.twig');
+    }
+
+    /**
+     * List of quote
+     *
+     * @Route("/admin/quotelist", name="quotelist")
+     */
+    public function quoteListAction()
+    {
+        return $this->render('@App/quoteList.html.twig');
+    }
+
+    /**
+     * Create invoice
+     *
+     * @Route("/admin/invoice", name="invoice")
+     */
+    public function createInvoiceAction()
+    {
+        return $this->render('@App/createInvoice.html.twig');
+    }
+
+    /**
+     * List of invoice
+     *
+     * @Route("/admin/invoicelist", name="invoicelist")
+     */
+    public function invoiceListAction()
+    {
+        return $this->render('@App/invoiceList.html.twig');
     }
 }
